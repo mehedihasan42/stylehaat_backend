2b7c7e5f78c06b40b7c4060362e8d65ae9560162
Dewan Mehedi Hasan <tanjildewan10@gmail.com>
2026-03-23 10:49:01 +0600

    fix authentication issue for orders

--- order/views.py
+++ order/views.py
@@ -19,7 +19,7 @@ from uuid import uuid4
 class OrdersList(ListCreateAPIView):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
-    authentication_classes = []
+    # authentication_classes = []
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
